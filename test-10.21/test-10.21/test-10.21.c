7c1d2549d83c7702904ca97d380d8877c8ee899e
Wang-Running <1767479196@qq.com>
2021-10-21 22:36:43 +0800

    三种循环代码对比

---
+++ test-10.21/test-10.21/test-10.21.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS
+//while do...while for 循环语句的比较
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	while (i < 10)
+//	{
+//		i++;
+//		sum = sum + i;
+//	}
+//	printf("%d",sum);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	i = 1;
+//	do
+//	{
+//		sum = sum + i;
+//		i++;
+//	} 
+//	while (i <=10);
+//	printf("%d",sum);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		sum = sum + i;
+//	}
+//	printf("%d",sum);
+//	return 0;
+//}
+//int MAX(int x,int y)
+//{
+//	if (x > y)
+//		return x;
+//	else
+//		return y;
+//}
+//
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int max = 0;
+//	scanf("%d %d",&a,&b);
+//	max = MAX(a,b);
+//	printf("%d\n",max);
+//	return 0;
+//}
+
+
+//数组使用
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	for (i = 0; i<10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//	return 0;
+//}
