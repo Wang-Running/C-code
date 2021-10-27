7b88d98ff944b1c3b413024350b56aaf7c40c0b8
Wang-Running <1767479196@qq.com>
2021-10-27 23:47:55 +0800

    最大值 乘法表 二分查找

--- test-10.26zy/test-10.26zy/test-10.26zy.c
+++ test-10.26zy/test-10.26zy/test-10.26zy.c
@@ -24,39 +24,84 @@
 //	float i = 0.0;
 //	float a = 0.0;
 //	float sum = 0;
+//	int b = 1;
 //	for (i = 1; i <= 100; i++)
 //	{
-//		a = 1.0 / i;
+//		a = 1.0 / i*b;
 //		sum += a;
+//		b = -b;
 //	}
 //	printf("%.2f\n",sum);
 //	return 0;
 //}
 
+
 //求10 个整数中最大值
 //#include <stdio.h>
 //int main()
 //{
-//	int arr[10] = {};
-//	scanf("%");
+//	int arr[10];
+//	int i = 0;
+//	for (i = 0; i < 10; i++)       //依次输入10个数
+//	{
+//		scanf("%d",&arr[i]);
+//	}
+//	int max = arr[0];
+//	for (i = 1; i < 10; i++)         //下标递增，依次比较
+//	{
+//		if (arr[i] > max)
+//			max = arr[i];
+//	}
+//	printf("%d",max);
 //	return 0;
 //}
 
 //在屏幕上输出9*9乘法口诀表
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i < 10; i++)               //九次行循环
+//	{
+//		int j = 0;
+//		int a = 0;
+//		for (j=1;j<=i;j++)
+//		{
+//
+//			printf("%d*%d=%2d ",j,i,i*j);
+//		}
+//		printf("\n");                    //一行完了换行
+//	}
+//	return 0;
+//}
+
+//二分查找 要求：找到了就打印数字所在的下标，找不到则输出：找不到。
 #include <stdio.h>
 int main()
 {
-	int i = 0;
-	for (i = 0; i < 9; i++)               //九次行循环
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int a = 1;
+	int left = 0;
+	int right = sizeof(arr) / sizeof(arr[0])-1;
+	while (left <= right)
 	{
-		int j = 0;
-		int a = 0;
-		for (j=0;j<i;j++)
+		int mid = (left + right) / 2;
+		if (arr[mid] < a)
 		{
-
-			printf("%d*%d=%d",);
+			left = mid+1;
 		}
-		printf("\n");                    //一行完了换行
+		else if (arr[mid]>a)
+		{
+			right = mid-1;
+		}
+		else
+		{
+			printf("找到了，下标是：%d\n", mid);
+			break;
+		}
 	}
+	if (left > right)
+		printf("找不到\n");
 	return 0;
 }
+
