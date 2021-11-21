663fcf21b09471fbc7d7166a26e6c6c26c1a14e5
Wang-Running <1767479196@qq.com>
2021-11-21 23:37:20 +0800

    调试实践，const修饰指针

---
+++ test-11.21zy/test-11.21zy/test-11.21zy.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+//分析程序死循环  ，有道云笔记
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		arr[i] = 0;
+//		printf("hello bit\n");
+//	}
+//	return 0;
+//}
+
+//模拟实现库函数strlen
+#include <stdio.h>
+//#include <string.h>
+
+//int main()
+//{
+//	char arr[] =  "abcdef" ;
+//	printf("%d\n",strlen(arr));
+//	return 0;
+//}
+
+int my_strlen(char* s)
+{
+	int i = 0;
+	while (*s!= '\0')
+	{
+		i++;
+		s++;
+	}
+	return i;
+}
+
+int main()
+{
+	char arr[] = "abcdef";
+	printf("%d\n",my_strlen(arr));
+	return 0;
+}
