736c088deeae3053371f5eb6ae29964174e01644
Wang-Running <1767479196@qq.com>
2021-11-26 23:57:25 +0800

    序列中删除指定数字

---
+++ test-11.25zy/test-11.25zy/test-11.25zy.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//序列中删除指定数字
+#include<stdio.h>
+void Dal(int *arr,int n,int b)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i<n; i++, j++)
+	{
+		if (arr[i] == b)
+		{
+			for (j; j < n; j++, i++)
+			{
+				arr[i] = arr[j + 1];
+				Dal(arr, n, b);
+			}
+			break;
+		}
+	}
+}
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int arr[30];
+	int b = 0;
+	int i = 0;
+	for (i = 0; i<n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	scanf("%d", &b);
+	Dal(arr,n,b);
+	for (i = 0; i<n - 1; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
