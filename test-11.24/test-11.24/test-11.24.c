0ffa265c52d445a0deef9c0da6eef6e00814907b
Wang-Running <1767479196@qq.com>
2021-12-01 00:08:12 +0800

    杨辉 猜凶手 跳水 台阶

--- test-11.24/test-11.24/test-11.24.c
+++ test-11.24/test-11.24/test-11.24.c
@@ -32,6 +32,30 @@
 //¡­¡­
 
 //#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int arr[10][10] = { 0 };
+//	scanf("%d",&i);
+//	for (i=0;i<10;i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j<=i; j++)
+//		{
+//			if (j == 0)
+//				arr[i][j] = 1;
+//			if (i == j)
+//				arr[i][j] = 1;
+//			if (i>1&&j>0)
+//				arr[i][j] = arr[i-1][j] + arr[i-1][j-1];
+//			printf("%d ",arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
 //
 //void yangHuiTriangle(int n)
 //{
@@ -195,7 +219,36 @@
 #include <stdio.h>
 int main()
 {
-
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int e = 0;
+	for (a = 1;a<=5;a++)
+	{
+		for (b=1;b<=5;b++)
+		{
+			for (c = 1;c<=5;c++)
+			{
+				for (d=1;d<=5;d++)
+				{
+					for (e = 1;e<=5;e++)
+					{
+						if (((b == 2) + (a == 3) == 1)
+							&& ((b == 2) + (e == 4) == 1)
+							&& ((c == 1) + (d == 2) == 1)
+							&& ((c == 5) + (d == 3) == 1)
+							&& ((e == 4) + (a == 1) == 1)
+							)
+						{
+							if (a*b*c*d*e==120)                             //·ÀÖØ¸´
+							printf("a=%d b=%d c=%d d=%d e=%d\n",a,b,c,d,e);
+						}
+					}
+				}
+			}
+		}
+	}
 	return 0;
 }
 
