2a795483cea79e6f1104a8ebc98b59dc6fcea72d
Wang-Running <1767479196@qq.com>
2022-01-21 18:33:33 +0800

    杨氏矩阵

--- test-1.15zy/test-1.15zy/test-1.15zy.c
+++ test-1.15zy/test-1.15zy/test-1.15zy.c
@@ -47,44 +47,85 @@
 //
 //要求：时间复杂度小于O(N);
 
+//#include <stdio.h>
+//
+//int findnum(int a[][3], int x, int y, int f) //第一个参数的类型需要调整
+//{
+//	int i = 0, j = x - 1; //从右上角开始遍历
+//
+//	while (j >= 0 && i < y)
+//	{
+//		if (a[i][j] < f) //比我大就向下
+//		{
+//			i++;
+//		}
+//		else if (a[i][j] > f) //比我小就向左
+//		{
+//			j--;
+//		}
+//		else
+//		{
+//			return 1;
+//		}
+//	}
+//	return 0;
+//}
+//
+//int main()
+//{
+//	int a[][3] = { { 1, 3, 5 },
+//	               { 3, 5, 7 },
+//	               { 5, 7, 9 } }; //一个示例
+//
+//	if (findnum(a, 3, 3, 8))
+//	{
+//		printf("It has been found!\n");
+//	}
+//	else
+//	{
+//		printf("It hasn't been found!\n");
+//	}
+//
+//	return 0;
+//}
+
+//字符串旋转结果
+//
+//写一个函数，判断一个字符串是否为另外一个字符串旋转之后的字符串。
+//
+//例如：给定s1 = AABCD和s2 = BCDAA，返回1
+//
+//给定s1 = abcd和s2 = ACBD，返回0.
+//
+//AABCD左旋一个字符得到ABCDA
+//
+//AABCD左旋两个字符得到BCDAA
+//
+//AABCD右旋一个字符得到DAABC
+
+
 #include <stdio.h>
-
-int findnum(int a[][3], int x, int y, int f) //第一个参数的类型需要调整
+#include <assert.h>
+char* findRound(const char * src, char * find)
 {
-	int i = 0, j = x - 1; //从右上角开始遍历
-
-	while (j >= 0 && i < y)
-	{
-		if (a[i][j] < f) //比我大就向下
-		{
-			i++;
-		}
-		else if (a[i][j] > f) //比我小就向左
-		{
-			j--;
-		}
-		else
-		{
-			return 1;
-		}
-	}
-	return 0;
+	assert(src);
+	char tmp[256] = { 0 }; //用一个辅助空间将原字符串做成两倍原字符串
+	strcpy(tmp, src); //先拷贝一遍
+	strcat(tmp, src); //再连接一遍
+	return (char*)strstr(tmp, find); //看看找不找得到
 }
-
 int main()
 {
-	int a[][3] = { { 1, 3, 5 },
-	               { 3, 5, 7 },
-	               { 5, 7, 9 } }; //一个示例
-
-	if (findnum(a, 3, 3, 8))
-	{
-		printf("It has been found!\n");
-	}
+	char arr1[20] = { 0 };
+	char arr2[20] = { 0 };
+	scanf("%s %s",arr1,arr2);
+	char* p;
+	p = findRound(arr1, arr2);
+	if (p!=NULL)
+		printf("1\n");
 	else
 	{
-		printf("It hasn't been found!\n");
+		printf("0\n");
 	}
-
 	return 0;
 }
