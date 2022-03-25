ccd2d2c302daeb398f6e89750494be6503ce5f35
Wang-Running <1767479196@qq.com>
2022-03-25 22:25:37 +0800

    栈的实现

---
+++ StackQueue/StackQueue/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "Stack.h"
+
+
+void TestStack()
+{
+	ST st;
+	StackInit(&st);
+	StackPush(&st,1);
+	StackPush(&st, 2);
+	StackPush(&st, 3);
+	StackPush(&st, 4);
+	StackPush(&st, 5);
+
+	while (!StackEmpty(&st))
+	{
+		printf("%d ",StackTop(&st));
+		StackPop(&st);
+	}
+
+	StackDestory(&st);
+}
+
+
+
+int main()
+{
+	TestStack();
+	return 0;
+}
