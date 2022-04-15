7c45ccb69cce0096e100aa67dca6aa6beb5c087a
Wang-Running <1767479196@qq.com>
2022-04-16 00:49:38 +0800

    二叉树基础OJ

--- test-4.12zy/test-4.12zy/test-4.12zy.c
+++ test-4.12zy/test-4.12zy/test-4.12zy.c
@@ -51,3 +51,93 @@
 //	return _isSymmetric(root->left, root->right);
 //}
 
+//力扣 144 二叉树的前序遍历
+//提前算出树的结点大小
+//int TreeSize(struct TreeNode* root)
+//{
+//	return root == NULL ? 0 : TreeSize(root->left) + TreeSize(root->right) + 1;
+//}
+//
+//void _preorderTraversal(struct TreeNode* root, int* a, int* i)
+//{
+//	if (root == NULL)
+//	{
+//		return;
+//	}
+//	//将数据放入数组中
+//	a[(*i)++] = root->val;
+//	_preorderTraversal(root->left, a, i);
+//	_preorderTraversal(root->right, a, i);
+//}
+//
+//int* preorderTraversal(struct TreeNode* root, int* returnSize){
+//	//开辟出对应的空间
+//	int* a = malloc(sizeof(int)*TreeSize(root));
+//	//避免重复开辟空间，定义子函数
+//	*returnSize = TreeSize(root);
+//	int i = 0;
+//	_preorderTraversal(root, a, &i);
+//	return a;
+//}
+
+//力扣 94 二叉树的中序遍历
+//提前算出树的结点大小
+//int TreeSize(struct TreeNode* root)
+//{
+//	return root == NULL ? 0 : TreeSize(root->left) + TreeSize(root->right) + 1;
+//}
+//
+//
+//void _inorderTraversal(struct TreeNode* root, int* a, int* i)
+//{
+//	if (root == NULL)
+//	{
+//		return;
+//	}
+//	_inorderTraversal(root->left, a, i);
+//	//将数据放入数组中
+//	a[(*i)++] = root->val;
+//	_inorderTraversal(root->right, a, i);
+//}
+//
+//
+//int* inorderTraversal(struct TreeNode* root, int* returnSize){
+//	//开辟出对应的空间
+//	int* a = malloc(sizeof(int)*TreeSize(root));
+//	//避免重复开辟空间，定义子函数
+//	*returnSize = TreeSize(root);
+//	int i = 0;
+//	_inorderTraversal(root, a, &i);
+//	return a;
+//}
+
+//力扣 145 二叉树的后序遍历
+//提前算出树的结点大小
+//int TreeSize(struct TreeNode* root)
+//{
+//	return root == NULL ? 0 : TreeSize(root->left) + TreeSize(root->right) + 1;
+//}
+//
+//
+//void _postorderTraversal(struct TreeNode* root, int* a, int* i)
+//{
+//	if (root == NULL)
+//	{
+//		return;
+//	}
+//	_postorderTraversal(root->left, a, i);
+//	_postorderTraversal(root->right, a, i);
+//	//将数据放入数组中
+//	a[(*i)++] = root->val;
+//}
+//
+//
+//int* postorderTraversal(struct TreeNode* root, int* returnSize){
+//	//开辟出对应的空间
+//	int* a = malloc(sizeof(int)*TreeSize(root));
+//	//避免重复开辟空间，定义子函数
+//	*returnSize = TreeSize(root);
+//	int i = 0;
+//	_postorderTraversal(root, a, &i);
+//	return a;
+//}
