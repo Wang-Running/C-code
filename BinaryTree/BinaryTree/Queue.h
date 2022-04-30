1271fd61c79b6961eb93f6302950c07a23d3fe86
Wang-Running <1767479196@qq.com>
2022-04-30 11:35:39 +0800

    二叉树遍历+基础练习

---
+++ BinaryTree/BinaryTree/Queue.h
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
+
+struct BinaryTreeNode;
+
+typedef struct BinaryTreeNode* QDatatype;
+
+//定义单链表结点
+typedef struct QueueNode
+{
+	QDatatype data;
+	struct QueueNode* next;
+}QNode;
+
+typedef struct Queue
+{
+	QNode* head;
+	QNode* tail;
+}Queue;
+
+void QueueInit(Queue* pq);
+
+void QueueDestory(Queue* pq);
+
+void QueuePush(Queue* pq,QDatatype x);
+
+void QueuePop(Queue* pq);
+
+bool QueueEmpty(Queue* pq);
+
+size_t QueueSize(Queue* pq);
+
+QDatatype QueueFront(Queue* pq);
+
+QDatatype QueueBack(Queue* pq);
